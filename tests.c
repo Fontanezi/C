64ef271f87b45ff04428c57157738597c955b844
Nt_partlycloudly <fontanezieducational@gmail.com>
2023-07-13 14:48:43 -0300

    New file

---
+++ tests.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NumeroMaxTimes 20
+#define NumeroMaxRodadas 38
+#define NumeroMaxJogos NumeroMaxTimes * NumeroMaxRodadas
+
+typedef struct auxtime {
+  int PontosGanhos;
+  int GolsMarcados;
+  int GolsSofridos;
+  int SaldoDeGols;
+  int Vitorias;
+  int Empates;
+  int Derrotas;
+  float GolAverage;
+  char nome[31];
+} time;
+
+typedef struct auxjogo {
+  char local[30];
+  char visitante[30];
+  int golslocal;
+  int golsvisitante;
+} jogo;
+
+void lenometime(char * s) {
+  int i;
+  s[0] = '\0';
+  char c;
+  c = getchar();
+  for(i = 0; c != ',' && i < 30;++i)
+    {
+      // if(c == '\n')
+      //   continue;
+      s[i] = c;
+      c = getchar();
+    }
+  s[i] = '\0';
+  //printf("%s\n",s);
+}
+
+
