f8b26dc6d0850432efe684a44737ba79babc7bcf
Nt_partlycloudly <fontanezieducational@gmail.com>
2023-07-18 12:41:02 -0300

    Edit code

--- EP2.c
+++ EP2.c
@@ -14,9 +14,10 @@
 
 #define NumeroMaxTimes 20
 #define NumeroMaxRodadas 38
-#define NumeroMaxJogos NumeroMaxTimes * NumeroMaxRodadas
+#define NumeroMaxJogos NumeroMaxTimes *NumeroMaxRodadas
 
-typedef struct auxtime {
+typedef struct auxtime
+{
   int PontosGanhos;
   int GolsMarcados;
   int GolsSofridos;
@@ -28,43 +29,46 @@ typedef struct auxtime {
   char nome[31];
 } time;
 
-typedef struct auxjogo {
+typedef struct auxjogo
+{
   char local[30];
   char visitante[30];
   int golslocal;
   int golsvisitante;
 } jogo;
 
-void lenometime(char * s) {
+void lenometime(char *s)
+{
   int i;
   s[0] = '\0';
   char c;
   c = getchar();
-  for(i = 0; c != ',' && i < 30;++i)
-    {
-      s[i] = c;
-      c = getchar();
-    }
+  for (i = 0; c != ',' && i < 30; ++i)
+  {
+    s[i] = c;
+    c = getchar();
+  }
   s[i] = '\0';
 }
 
 // Ordena por Pontos
 
-void ordenaPontos(time * timescampeonato, int notimes)
+void ordenaPontos(time *timescampeonato, int notimes)
 {
   // Adicione seu código para ordenar em ordem decrescente de pontos ganhos
 }
 
 // Ordena por saldo de gols
 
-void ordenaSaldoGols(time * timescampeonato, int notimes)
+void ordenaSaldoGols(time *timescampeonato, int notimes)
 {
   // Adicione seu código para ordenar em ordem decrescente de saldo de gols
 }
 
 // Encontra se o time já está no arranjo de times do campeonato
 
-int encontratime(time *timescampeonato, char * nome, int notimes) {
+int encontratime(time *timescampeonato, char *nome, int notimes)
+{
   // Retorna a posição do arranjo times de campeonato
   // Adicione seu código
 
@@ -73,69 +77,92 @@ int encontratime(time *timescampeonato, char * nome, int notimes) {
 
 // Cria a lista de times do campeonato a partir dos nojogos
 
-int crialistatimes(time * timescampeonato, jogo * dadosjogos, int numerojogos) {
+int crialistatimes(time *timescampeonato, jogo *dadosjogos, int numerojogos)
+{
   // Preenche o arranjo timescampeonato com os times
-  // Adicione seu código
-
+  for (int i = 0; i < numerojogos; i++)
+  {
+    for (int j = 0; j < numerojogos; j += 2)
+    {
+      timescampeonato[j].nome = dadosjogos[i].local;
+      timescampeonato[j + 1].nome = dadosjogos[i].visitante;
+      if (dadosjogos[i].golslocal > dadosjogos[i].golsvisitante)
+      {
+        timescampeonato[j].Vitorias += 1;
+        timescampeonato[j + 1].Derrotas += 1;
+      }
+      else if (dadosjogos[i].golsvisitante > dadosjogos[i].golslocal)
+      {
+        timescampeonato[j + 1].Vitorias += 1;
+        timescampeonato[j].Derrotas += 1;
+      }
+      else
+      {
+        timescampeonato[j].Empates += 1;
+        timescampeonato[j + 1].Empates += 1;
+      }
+    }
+  }
   return 0; // retorna o número de times. O zero é só para compilar
 }
 
 // Computa dados times
 
-void computadadostimes(time * timescampeonato,int notimes,jogo * dadosjogos,int numerojogos) {
-// Preenche os campos dos elementos do arranjo timescampeonato: Vitorias,
-// GolsSofridos, GolsMarcados, Golsaverage, SaldoDeGols, PontosGanhos.
-// Adicione seu código
-
- }
+void computadadostimes(time *timescampeonato, int notimes, jogo *dadosjogos, int numerojogos)
+{
+  // Preenche os campos dos elementos do arranjo timescampeonato: Vitorias,
+  // GolsSofridos, GolsMarcados, Golsaverage, SaldoDeGols, PontosGanhos.
+  // Adicione seu código
+}
 
 // Imprime classificação
 
-void imprimeclassificacao(time * timescampeonato,int notimes){
- // Adicione seu código
-
+void imprimeclassificacao(time *timescampeonato, int notimes)
+{
+  // Adicione seu código
 }
 
 // Salva os dados da classificação dos tipos em arquivo no disco
 
-void salvaclassificacao(time * timescampeonato,int notimes, char * arquivo){
-// Adicione seu código
-
+void salvaclassificacao(time *timescampeonato, int notimes, char *arquivo)
+{
+  // Adicione seu código
 }
 
-int main() {
+int main()
+{
   time times[NumeroMaxTimes];
   jogo jogos[NumeroMaxJogos];
   int i, nojogos, golslocal = 0, golsvisitante = 0;
-  char local[30],visitante[30];
+  char local[30], visitante[30];
 
   printf("Entre os jogos no formato \"time local, time visitante, golslocal, golsvisitante\" (gols local negativo encerra a entrada de dados)\n");
-  for(i = 0; i < NumeroMaxJogos; ++i) {
+  for (i = 0; i < NumeroMaxJogos; ++i)
+  {
     lenometime(local);
     lenometime(visitante);
-    scanf("%d,%d",&golslocal,&golsvisitante);
-    getchar(); 
+    scanf("%d,%d", &golslocal, &golsvisitante);
+    getchar();
 
     if (golslocal < 0)
-      break; 
+      break;
 
-    if(strcmp(local,visitante) == 0)
-      continue; 
+    if (strcmp(local, visitante) == 0)
+      continue;
 
-    strncpy(jogos[i].local,local,30);
-    strncpy(jogos[i].visitante,visitante,30);
+    strncpy(jogos[i].local, local, 30);
+    strncpy(jogos[i].visitante, visitante, 30);
 
     jogos[i].golslocal = golslocal;
     jogos[i].golsvisitante = golsvisitante;
   }
   nojogos = i;
 
-  int notimes = crialistatimes(times,jogos,nojogos);
+  int notimes = crialistatimes(times, jogos, nojogos);
 
-  computadadostimes(times,notimes,jogos,nojogos);
+  computadadostimes(times, notimes, jogos, nojogos);
 
-  imprimeclassificacao(times,notimes);
-  
-  salvaclassificacao(times,notimes,"campeonatoIP.dat");
+  imprimeclassificacao(times, notimes);
 
+  salvaclassificacao(times, notimes, "campeonatoIP.dat");
 }
