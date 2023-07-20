19d52da7562acf5273dfc52264f5029b75e30e32
Nt_partlycloudly <fontanezieducational@gmail.com>
2023-07-19 23:11:10 -0300

    Final Commit

--- EP2.c
+++ EP2.c
@@ -1,14 +1,3 @@
-/*********************************************************************/
-/**   ACH2001 - Introdução à Programação                            **/
-/**   EACH-USP - Primeiro Semestre de 2023                          **/
-/**   Turma 02 - Prof. Marcos Lordello Chaim                        **/
-/**                                                                 **/
-/**   Segundo Exercicio-Programa                                    **/
-/**                                                                 **/
-/**   João Paulo Fernandes Fontanezi                  14747191      **/
-/**                                                                 **/
-/*********************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
