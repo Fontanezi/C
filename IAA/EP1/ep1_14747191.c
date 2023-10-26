e1eeac72fbc71b11c10090e373ef9623238bcc1c
Nt_partlycloudly <fontanezieducational@gmail.com>
2023-10-26 20:00:16 -0300

    big changes

---
+++ IAA/EP1/ep1_14747191.c
@@ -0,0 +1,12 @@
+// EACH USP - SISTEMAS DE INFORMAÇÃO - IAA - SEGUNDO SEMESTRE DE 2023 - EP1
+// JOÃO PAULO FERNANDES FONTANEZI - 14747191
+
+#include <stdio.h>
+
+int N;
+
+int main()
+{
+
+    return 0;
+}
