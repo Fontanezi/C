1c8516298448a172c952da14b60604c41c1c546d
Nt_partlycloudly <fontanezieducational@gmail.com>
2023-11-01 15:34:31 -0300

    Semanal

---
+++ IP/AtividadeSemanal03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int meuNumeroUSP = 14747191;
+    printf("meuNumeroUSP\n");
+    printf("%i\n", meuNumeroUSP);
+    printf("%i\n", meuNumeroUSP / 5);
+    printf("%i\n", meuNumeroUSP % 5);
+    return 0;
+}
