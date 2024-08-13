0eaa524cfac8d1eaa0ec4200fba127f7734e14f4
Fontanezi <joao.fontanezi@usp.br>
2024-08-13 14:59:33 -0300

    Add insertionSort

---
+++ Introduction to Algorithm Analysis/insertionSort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int arr[] = {2, 5, 6, 3, 4, 1};
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    insertionSort(arr, 6);
+    return 0;
+}
